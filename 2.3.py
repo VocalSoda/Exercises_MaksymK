e7e671aaeb416af7151b07299f41920a32462faf
Maks K <157726925+VocalSoda@users.noreply.github.com>
2026-08-20 09:39:14 +0300

    Excercise 2.3 completed with additional type check

--- 2.3.py
+++ 2.3.py
@@ -2,13 +2,11 @@ a = input("side a: ");
 b = input("side b: ");
 
 
-if  int(a) and int(b) > 0:
-
+try:
     perm = 2*int(a) + 2*int(b)
     area = int(a)*int(b)
     print("perimeter is: " +  str(perm))
     print("area is: " +  str(area))
-
-else:
-
-    print("Please insert integer")
+except:
+    print("a and b should be numbers")
+    quit()
