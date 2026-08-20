fc4cc50e7dac2e8e80549a976f78688bb15d0801
Maks K <157726925+VocalSoda@users.noreply.github.com>
2026-08-20 13:29:28 +0300

    teht3 finished

--- muduli1 and 2/Tuntitehtävä 2/Teht3.py
+++ muduli1 and 2/Tuntitehtävä 2/Teht3.py
@@ -1,8 +1,10 @@
+import math
+
 grams = int(input("How many grams?: "))
 
 if grams > 1000:
     gtokg = grams / 1000
     dot = str(gtokg).find(".")
-    print("maara kiloina: " + str(gtokg)[:dot] + " grams: " + str(gtokg)[dot:])
+    print("maara kiloina: " + str(gtokg)[:dot] + " grams: " + str(math.floor(((gtokg - int(gtokg)) * 1000))))
 else:
     print("grams: " + str(grams))
