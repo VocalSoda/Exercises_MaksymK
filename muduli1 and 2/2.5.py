d711843524884824db8c9fdf620f064d6a6017c4
Maks K <157726925+VocalSoda@users.noreply.github.com>
2026-08-20 13:16:30 +0300

    2.5 tehty

--- muduli1 and 2/2.5.py
+++ muduli1 and 2/2.5.py
@@ -1,3 +1,5 @@
+import math
+
 talents = input("please enter the amount of talents: ");
 pounds = input("please enter the amount of pounds: ");
 lots = input("please enter the amount of lots: ");
@@ -13,7 +15,7 @@ try:
         summ = (int(talents)*kgtal) + (int(pounds)*kgpound) + (int(lots)*kglot)
         sumstr = str(summ)
         dot = sumstr.find(".")
-        print("In modern units " + str(summ)[:dot] + " Kilograms" + " and " +  str((summ - int(summ)) * 1000) + " grams")
+        print("In modern units " + str(summ)[:dot] + " Kilograms" + " and " +  str(math.floor(((summ - int(summ)) * 1000))) + " grams")
    
 except ValueError:
     print("please enter numbers");
